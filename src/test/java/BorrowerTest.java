56f808f21426ea004d415f40833a120f953f3ecf
Diane Jonker <Diane.eugenie.jonker@gmail.com>
2019-09-24 16:33:04 +0100

    Adds Borrower Class and tests if book collection starts empty

--- src/test/java/BorrowerTest.java
+++ src/test/java/BorrowerTest.java
@@ -1,4 +1,31 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertNotNull;
+import static org.junit.Assert.assertEquals;
+
 public class BorrowerTest {
+
+    Borrower borrower;
+    Book book;
+
+    @Before
+    public void before(){
+        borrower = new Borrower("Luna");
+        book = book = new Book("Fantastic Beasts And Where To Find Them", "Newt Scamander", "Magizoology");
+    }
+
+    @Test
+    public void hasNoBooksBeforeBorrowing(){
+        //Given we have a borrower
+        assertNotNull(borrower);
+        //AND a book
+        assertNotNull(book);
+        //When borrower goes to library
+        //Borrower has no book in collection
+        assertEquals(0, borrower.collectionCount());
+    }
+
 
 
 }
